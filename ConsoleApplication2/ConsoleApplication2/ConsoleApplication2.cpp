4bb4442737f49ff4aa439543b1f5d7bec999c915
Wandestes <gilua5568@gmail.com>
2025-03-05 11:15:05 +0200

    [Create project]
    Add project with Node

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <stdexcept>
+
+class Node {
+public:
+    char data;
+    Node* next;
+    Node* prev;
+
+    Node(char data) : data(data), next(nullptr), prev(nullptr) {}
+};
+
