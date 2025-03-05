3f7335f93234670ff108e897d60d2af91445e147
Wandestes <gilua5568@gmail.com>
2025-03-05 11:17:19 +0200

    [Commit 2] Update ConsoleApplication2.cpp
    Add dll, append and insert (Classes)

--- ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <stdexcept>
 
 class Node {
@@ -10,3 +10,54 @@ public:
     Node(char data) : data(data), next(nullptr), prev(nullptr) {}
 };
 
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
+    int size;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+    ~DoublyLinkedList() { clear(); }
+
+    int length() { return size; }
+
+    void append(char element) {
+        Node* newNode = new Node(element);
+        if (!head) {
+            head = tail = newNode;
+        }
+        else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    void insert(char element, int index) {
+        if (index < 0 || index > size) {
+            throw std::out_of_range("Invalid index");
+        }
+        Node* newNode = new Node(element);
+        if (index == 0) {
+            newNode->next = head;
+            if (head) head->prev = newNode;
+            head = newNode;
+            if (!tail) tail = newNode;
+        }
+        else if (index == size) {
+            append(element);
+            return;
+        }
+        else {
+            Node* temp = head;
+            for (int i = 0; i < index - 1; i++) temp = temp->next;
+            newNode->next = temp->next;
+            newNode->prev = temp;
+            temp->next->prev = newNode;
+            temp->next = newNode;
+        }
+        size++;
+    }
