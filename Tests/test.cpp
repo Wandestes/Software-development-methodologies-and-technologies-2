bedc63eb7ac84b0cc762a9273a61bda32e90679e
Wandestes <gilua5568@gmail.com>
2025-03-05 12:37:07 +0200

    Update test.cpp

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include "../Catch/catch_amalgamated.hpp"
-#include "DoublyLinkedList.h"
+#include "#include "../ConsoleApplication2/DoublyLinkedList.h"
 
 TEST_CASE("DoublyLinkedList: Append and Length") {
     DoublyLinkedList list;
