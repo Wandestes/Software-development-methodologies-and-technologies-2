23893d4e7947040a8efbea64cafea4251979eca2
Wandestes <gilua5568@gmail.com>
2025-03-05 11:56:44 +0200

    Update test.cpp

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -24,7 +24,7 @@ TEST_CASE("DoublyLinkedList: Insert") {
     
     list.insert('B', 1);
     REQUIRE(list.length() == 3);
-    REQUIRE(list.get(1) == 'Z');
+    REQUIRE(list.get(1) == 'Z'); 
     REQUIRE(list.get(2) == 'C');
 }
 
@@ -61,4 +61,50 @@ TEST_CASE("DoublyLinkedList: FindFirst and FindLast") {
 
     REQUIRE(list.findFirst('B') == 1);  
     REQUIRE(list.findLast('B') == 3);   
+}
+
+TEST_CASE("DoublyLinkedList: Edge Cases - Empty List") {
+    DoublyLinkedList list;
+    REQUIRE(list.length() == 0);
+    
+
+    REQUIRE_THROWS_AS(list.get(0), std::out_of_range);  
+    REQUIRE_THROWS_AS(list.deleteAt(0), std::out_of_range);  
+}
+
+TEST_CASE("DoublyLinkedList: Edge Cases - Out of Bound") {
+    DoublyLinkedList list;
+    list.append('A');
+    list.append('B');
+    
+    // Перевірка на вихід за межі списку
+    REQUIRE_THROWS_AS(list.get(2), std::out_of_range); 
+    REQUIRE_THROWS_AS(list.deleteAt(3), std::out_of_range);  
+
+TEST_CASE("DoublyLinkedList: Clone") {
+    DoublyLinkedList list;
+    list.append('A');
+    list.append('B');
+
+    DoublyLinkedList clonedList = list.clone(); 
+
+    REQUIRE(clonedList.length() == 2);
+    REQUIRE(clonedList.get(0) == 'A');
+    REQUIRE(clonedList.get(1) == 'B');
+}
+
+TEST_CASE("DoublyLinkedList: Extend") {
+    DoublyLinkedList list;
+    list.append('A');
+    list.append('B');
+
+    DoublyLinkedList listToExtend;
+    listToExtend.append('C');
+    listToExtend.append('D');
+
+    list.extend(listToExtend);  
+
+    REQUIRE(list.length() == 4);
+    REQUIRE(list.get(2) == 'C');
+    REQUIRE(list.get(3) == 'D');
 }
